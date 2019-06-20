6201a3629d5757751b107bb9be790a9afcf4fcea
David McNett <nugget@macnugget.org>
2019-06-20 15:25:40 -0500

    Refactor minecraft server connection handling
    This commit re-works how the Minecraft mcserver lib handles the
    connection to the server in order to more gracefully handle disconnects
    and reconnects.

--- lib/mcserver/main.go
+++ lib/mcserver/main.go
@@ -5,6 +5,8 @@ import (
 	"strings"
 	"time"
 
+	"github.com/nugget/phoebot/lib/ipc"
+
 	"github.com/Tnze/go-mc/authenticate"
 	"github.com/Tnze/go-mc/bot"
 	"github.com/Tnze/go-mc/chat"
@@ -13,11 +15,16 @@ import (
 	"github.com/tidwall/gjson"
 )
 
-var (
-	Client     *bot.Client
-	remoteHost string
-	remotePort int
-)
+type Server struct {
+	Client      *bot.Client
+	Hostname    string
+	Port        int
+	Email       string
+	password    string
+	Connected   bool
+	ConnectTime time.Time
+	authData    authenticate.Response
+}
 
 type PingStats struct {
 	Delay         time.Duration
@@ -28,69 +35,238 @@ type PingStats struct {
 	Protocol      int64
 }
 
-func LogFields(f logrus.Fields) logrus.Fields {
+func New() (Server, error) {
+	s := Server{}
+	s.Client = bot.NewClient()
+	return s, nil
+}
+
+func (s *Server) Authenticate(hostname string, port int, email, password string) error {
+	s.Hostname = hostname
+	s.Port = port
+	s.Email = email
+	s.password = password
+
+	return nil
+}
+
+func (s *Server) HandleGame() error {
+	logrus.Debug("Minecraft HandleGame Started")
+	err := s.Client.HandleGame()
+	logrus.WithError(err).Error("Minecraft HandleGame Exited")
+
+	s.Connected = false
+	return err
+}
+
+func (s *Server) Connect() (err error) {
+	logrus.WithFields(logrus.Fields{
+		"token":       s.authData.AccessToken,
+		"connectTime": s.ConnectTime,
+		"connected":   s.Connected,
+	}).Trace("mcserver Connect()")
+
+	if s.Connected {
+		return nil
+	}
+
+	if s.authData.AccessToken == "" {
+		s.authData, err = authenticate.Authenticate(s.Email, s.password)
+		if err != nil {
+			return err
+		}
+		logrus.WithFields(logrus.Fields{
+			"name":  s.authData.SelectedProfile.Name,
+			"uuid":  s.authData.SelectedProfile.ID,
+			"token": s.authData.AccessToken,
+		}).Info("Authenticated with mojang")
+
+		s.Client.Name, s.Client.Auth.UUID, s.Client.AsTk = s.authData.SelectedProfile.Name, s.authData.SelectedProfile.ID, s.authData.AccessToken
+	}
+
+	err = s.Client.JoinServer(s.Hostname, s.Port)
+	if err != nil {
+		s.Connected = false
+		return err
+	}
+
+	logrus.WithFields(logrus.Fields{
+		"hostname": s.Hostname,
+		"port":     s.Port,
+		"uuid":     s.Client.Auth.UUID,
+		"name":     s.Client.Auth.Name,
+	}).Info("Connected to Minecraft server")
+
+	s.Connected = true
+	s.ConnectTime = time.Now()
+
+	s.Client.Events.GameStart = s.OnGameStart
+	s.Client.Events.ChatMsg = s.OnChatMsg
+	s.Client.Events.Disconnect = s.OnDisconnect
+	s.Client.Events.PluginMessage = s.OnPluginMessage
+	s.Client.Events.Die = s.OnDieMessage
+
+	go s.HandleGame()
+
+	return nil
+}
+
+func (s *Server) TestConnection() error {
+	if s.Connected == false {
+		return fmt.Errorf("We are not connected")
+	}
+
+	inventory := s.Client.MainInventory()
+	err := s.Client.SwingArm(0)
+
+	//fmt.Printf("Client: %+v\n", Client)
+	//fmt.Printf("PlayInfo: %+v\n", Client.PlayInfo)
+	//fmt.Printf("Chunks: %d\n", len(Client.Wd.Chunks))
+	//fmt.Printf("inv: %+v\n", inventory)
+
+	logrus.WithFields(logrus.Fields{
+		"slot0":      inventory[0],
+		"chunkCount": len(s.Client.Wd.Chunks),
+		"gamemode":   s.Client.PlayInfo.Gamemode,
+		"dimension":  s.Client.PlayInfo.Dimension,
+		"difficulty": s.Client.PlayInfo.Difficulty,
+		"swingarm":   err,
+	}).Trace("TestConnection")
+
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"slot0":      inventory[0],
+			"chunkCount": len(s.Client.Wd.Chunks),
+			"gamemode":   s.Client.PlayInfo.Gamemode,
+			"dimension":  s.Client.PlayInfo.Dimension,
+			"difficulty": s.Client.PlayInfo.Difficulty,
+			"swingarm":   err,
+		}).Debug("TestConnection")
+		return fmt.Errorf("Can't swing my arm: %v", err)
+	}
+
+	return nil
+}
+
+func (s *Server) LogFields(f logrus.Fields) logrus.Fields {
 	if f == nil {
 		f = logrus.Fields{}
 	}
 
-	conn := Client.Conn()
+	conn := s.Client.Conn()
 
-	f["name"] = Client.Auth.Name
+	f["name"] = s.Client.Auth.Name
 	f["server"] = conn.Socket.RemoteAddr()
 
 	return f
 }
 
-func Login(hostname string, port int, email, password string) error {
-	Client = bot.NewClient()
+func (s *Server) Handler() {
+	backoffSeconds := 10
+	backoffLimit := 300
+	retries := 1
 
-	auth, err := authenticate.Authenticate(email, password)
+	for {
+		logrus.Trace("Handler loop")
+
+		err := s.Connect()
+		if err == nil {
+			// Reset the backoff after a successful connection
+			backoffSeconds = 5
+			retries = 1
+		} else {
+			retries++
+			if backoffSeconds < backoffLimit {
+				backoffSeconds = int(float64(backoffSeconds) * 1.5)
+			}
+
+			logrus.WithFields(logrus.Fields{
+				"error":          err,
+				"retries":        retries,
+				"backoffSeconds": backoffSeconds,
+				"backoffLimit":   backoffLimit,
+			}).Error("Unable to connect to Minecraft server")
+		}
+
+		time.Sleep(time.Duration(backoffSeconds) * time.Second)
+
+		err = s.TestConnection()
+		if err != nil {
+			logrus.WithError(err).Warn("TestConnection")
+			s.Connected = false
+		}
+	}
+}
+
+func (s *Server) Status() (ps PingStats, err error) {
+	var resp []byte
+
+	resp, ps.Delay, err = bot.PingAndList(s.Hostname, s.Port)
 	if err != nil {
-		return err
+		return ps, err
 	}
 
-	logrus.WithFields(logrus.Fields{
-		"name":  auth.SelectedProfile.Name,
-		"uuid":  auth.SelectedProfile.ID,
-		"token": auth.AccessToken,
-	}).Debug("Authenticated with mojang")
+	json := string(resp)
 
-	Client.Name, Client.Auth.UUID, Client.AsTk = auth.SelectedProfile.Name, auth.SelectedProfile.ID, auth.AccessToken
+	ps.PlayersOnline = gjson.Get(json, "players.online").Int()
+	ps.PlayersMax = gjson.Get(json, "players.max").Int()
+	ps.Description = gjson.Get(json, "description.text").String()
+	ps.Version = gjson.Get(json, "version.name").String()
+	ps.Protocol = gjson.Get(json, "verison.protocol").Int()
 
-	err = Client.JoinServer(hostname, port)
-	if err != nil {
-		return err
+	ps.PlayersOnline--
+
+	return ps, nil
+}
+
+func (s *Server) OnGameStart() error {
+	logrus.WithFields(s.LogFields(nil)).Info("Minecraft start")
+	return nil //if err isn't nil, HandleGame() will return it.
+}
+
+func (s *Server) OnChatMsg(c chat.Message, pos byte) error {
+	cleanMessage := CleanString(c)
+
+	f := s.LogFields(logrus.Fields{
+		"pos":   pos,
+		"event": "chat",
+	})
+
+	if ipc.ServerChatStream != nil {
+		ipc.ServerChatStream <- c
+	} else {
+		logrus.WithFields(f).Info(cleanMessage)
 	}
-
-	remoteHost = hostname
-	remotePort = port
 
 	return nil
 }
 
-func Reconnect() {
-	interval := 30
-	retries := 1
+func (s *Server) OnDisconnect(c chat.Message) error {
+	logrus.WithFields(s.LogFields(logrus.Fields{
+		"message": CleanString(c),
+	})).Info("Minecraft disconnect")
 
-	for {
-		err := Client.JoinServer(remoteHost, remotePort)
-		if err == nil {
-			logrus.WithFields(LogFields(logrus.Fields{
-				"retries": retries,
-			})).Info("Minecraft reconnected")
+	s.Connected = false
 
-			go Handler()
+	return nil
+}
 
-			return
-		}
-		logrus.WithFields(logrus.Fields{
-			"error":   err,
-			"retries": retries,
-		}).Info("mcserver Reconnect Loop")
-		retries++
+func (s *Server) OnPluginMessage(channel string, data []byte) error {
+	logrus.WithFields(s.LogFields(logrus.Fields{
+		"channel": channel,
+		"data":    data,
+		"string":  string(data),
+	})).Info("Minecraft Plugin Message")
+	return nil
+}
 
-		time.Sleep(time.Duration(interval) * time.Second)
+func (s *Server) OnDieMessage() error {
+	logrus.WithFields(s.LogFields(nil)).Info("Minecraft death, respawning")
+	err := s.Client.Respawn()
+	if err != nil {
+		logrus.WithError(err).Error("Respawn failed")
 	}
+	return nil
 }
 
 func CleanString(m chat.Message) string {
@@ -167,107 +343,4 @@ func ChatMsgClass(m chat.Message) string {
 	}
 
 	return "other"
-}
-
-func Handler() error {
-	logrus.Debug("Minecraft Handler Launched")
-	go Heartbeat()
-	err := Client.HandleGame()
-	logrus.WithError(err).Error("Minecraft Handler Exited")
-	go Reconnect()
-	return err
-}
-
-func Heartbeat() {
-	for {
-		time.Sleep(time.Duration(60) * time.Second)
-
-		if len(Client.Wd.Chunks) == 0 {
-			logrus.Warn("No chunks, running reconnect")
-			go Reconnect()
-		} else {
-			inventory := Client.MainInventory()
-
-			//fmt.Printf("Client: %+v\n", Client)
-			//fmt.Printf("PlayInfo: %+v\n", Client.PlayInfo)
-			//fmt.Printf("Chunks: %d\n", len(Client.Wd.Chunks))
-			//fmt.Printf("inv: %+v\n", inventory)
-
-			logrus.WithFields(logrus.Fields{
-				"slot0":      inventory[0],
-				"chunks":     len(Client.Wd.Chunks),
-				"gamemode":   Client.PlayInfo.Gamemode,
-				"dimension":  Client.PlayInfo.Dimension,
-				"difficulty": Client.PlayInfo.Difficulty,
-			}).Debug("Heartbeat Loop")
-
-		}
-	}
-}
-
-func GetPingStats() (ps PingStats, err error) {
-	var resp []byte
-
-	resp, ps.Delay, err = bot.PingAndList(remoteHost, remotePort)
-	if err != nil {
-		return ps, err
-	}
-
-	json := string(resp)
-
-	ps.PlayersOnline = gjson.Get(json, "players.online").Int()
-	ps.PlayersMax = gjson.Get(json, "players.max").Int()
-	ps.Description = gjson.Get(json, "description.text").String()
-	ps.Version = gjson.Get(json, "version.name").String()
-	ps.Protocol = gjson.Get(json, "verison.protocol").Int()
-
-	ps.PlayersOnline--
-
-	return ps, nil
-}
-
-func OnGameStart() error {
-	logrus.WithFields(LogFields(nil)).Info("Minecraft start")
-	return nil //if err isn't nil, HandleGame() will return it.
-}
-
-func OnChatMsg(c chat.Message, pos byte) error {
-	cleanMessage := CleanString(c)
-
-	f := LogFields(logrus.Fields{
-		"pos":   pos,
-		"event": "chat",
-	})
-
-	logrus.WithFields(f).Info(cleanMessage)
-
-	return nil
-}
-
-func OnDisconnect(c chat.Message) error {
-	logrus.WithFields(LogFields(logrus.Fields{
-		"message": CleanString(c),
-	})).Info("Minecraft disconnect")
-
-	go Reconnect()
-
-	return nil
-}
-
-func OnPluginMessage(channel string, data []byte) error {
-	logrus.WithFields(LogFields(logrus.Fields{
-		"channel": channel,
-		"data":    data,
-		"string":  string(data),
-	})).Info("Minecraft Plugin Message")
-	return nil
-}
-
-func OnDieMessage() error {
-	logrus.WithFields(LogFields(nil)).Info("Minecraft death, respawning")
-	err := Client.Respawn()
-	if err != nil {
-		logrus.WithError(err).Error("Respawn failed")
-	}
-	return nil
 }
