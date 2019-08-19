8100158da893f89f4c394fc70d2d4cfabca47ae2
David McNett <nugget@macnugget.org>
2019-08-19 17:36:24 -0500

    Drop the index completely

--- db/deploy/mapowner.sql
+++ db/deploy/mapowner.sql
@@ -5,6 +5,4 @@ BEGIN;
     ALTER TABLE map ADD COLUMN owner varchar NOT NULL DEFAULT 'MacNugget';
     ALTER TABLE poi ADD COLUMN owner varchar NOT NULL DEFAULT 'MacNugget';
 
-    CREATE UNIQUE INDEX poi_index ON poi(class, description);
-
 COMMIT;
