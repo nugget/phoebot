164301a7fad437be7602e65114bb3073a86c2497
David McNett <nugget@macnugget.org>
2019-12-08 13:31:26 -0600

    New type field for scanranges

--- db/deploy/postal-tables.sql
+++ db/deploy/postal-tables.sql
@@ -2,13 +2,14 @@
 
 BEGIN;
 
-    CREATE TABLE postal_scan (
+    CREATE TABLE scanrange (
         scanrangeID uuid NOT NULL DEFAULT gen_random_uuid(),
         added timestamp(0) NOT NULL DEFAULT current_timestamp,
         changed timestamp(0) NOT NULL DEFAULT current_timestamp,
         deleted timestamp(0),
         lastscan timestamp(0) NOT NULL DEFAULT current_timestamp,
         enabled boolean NOT NULL DEFAULT TRUE,
+        scantype varchar NOT NULL DEFAULT 'mailboxes',
         name varchar NOT NULL DEFAULT '',
         dimension varchar DEFAULT 'overworld',
         sx int,
